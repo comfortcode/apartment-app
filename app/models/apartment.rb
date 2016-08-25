cc52e4f847f930ead4e343487b36c674f5cc45ad
comfortcode <nechamabgoldman@gmail.com>
2016-08-25 11:32:09 +0000

    added apt model to require additional params

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -5,7 +5,7 @@ class Apartment < ActiveRecord::Base
     belongs_to :rental_type
     belongs_to :price_time    
     has_many :images
-    validates :area_id, :street, :rental_type_id, :currency_id, :price_time, :price, :beds, :baths, presence: true
+    validates :area_id, :street, :rental_type_id, :currency_id, :price_time, :price, :beds, :baths, :furnished, :from_date, presence: true
     default_scope { order('created_at DESC') }    
     attr_accessor :available_now
 end
